0a186b5201b41c62de71fcd6870fd832f5d3e35d
chubbson <chubbson@gmail.com>
2010-11-28 19:08:36 +0100

    =Removed some tmpFiles

--- src/main/java/ch/hszt/hs_2010/mdp/HGHM/RunHello.java
+++ src/main/java/ch/hszt/hs_2010/mdp/HGHM/RunHello.java
@@ -1,7 +1,6 @@
 package ch.hszt.hs_2010.mdp.HGHM;
 
 public class RunHello {
-
 	public static void main(String[] args) {
 		Hello m_Hello = new Hello();
 		System.out.println(m_Hello.getHelloKitty());
