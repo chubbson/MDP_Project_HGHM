bf9f66d1a8f6c6e168aa70925947d5f3c0bbbebb
chubbson <chubbson@gmail.com>
2010-12-23 15:48:32 +0100

    Programm konstanten

---
+++ src/main/java/ch/hszt/hs_2010/mdp/HGHM/Common.java
@@ -0,0 +1,12 @@
+package main.java.ch.hszt.hs_2010.mdp.HGHM;
+
+public class Common {
+	public static final String APPLICATION_NAME = "Traceroute Analyzer";
+	public static final String DEFAULT_TRACEROUTE = 
+		"traceroute to oas.oagr.ch (217.26.54.155), 64 hops max, 52 byte packets\n"+
+		"1  10.0.2.254 (10.0.2.254)  1.305 ms  0.932 ms  0.996 ms\n"+
+		"2  * * *\n" + 
+		"3  217-168-60-117.static.cablecom.ch (217.168.60.117)  14.055 ms  10.465 ms  11.452 ms\n"+
+		"4  * 172.31.208.73 (172.31.208.73)  18.180 ms  12.778 ms\n"+
+		"5  213.46.171.66 (213.46.171.66)  15.087 ms  11.027 ms  22.585 ms";
+}
